bee4c12681b90e07b00fe01d05593eb024bf1985
HEMALATHA <hemalathamuthukrishnan586@gmail.com>
2017-09-15 15:08:46 +0530

    Create code.c

---
+++ code.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+ int a;
+ scanf("%d",&a);
+ if(a>=0)
+ {
+ printf("positive");
+ }
+ if(a<0)
+ {
+ printf("negative");
+ }
+ if(a==0)
+ {
+ printf("zero");
+ }
+ }
