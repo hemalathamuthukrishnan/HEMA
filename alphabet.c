d255bad8eecde4fbd8aa5537e42e028d0c059c63
HEMALATHA <hemalathamuthukrishnan586@gmail.com>
2017-09-15 16:00:45 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ char c;
+ scanf("%c",&c);
+  if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
+  {
+        printf("alphabet");
+  }
+  else
+  {
+     printf("alphabet");
+  }  
+}
