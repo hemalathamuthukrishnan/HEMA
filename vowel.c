1d7412fba7e9cd4d76d1b1ca9899d27dc4c7f5d8
HEMALATHA <hemalathamuthukrishnan586@gmail.com>
2017-09-15 15:40:15 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ char c;
+ scanf("%c",&c);
+ if('A'==c || 'E'==c || 'I'==c || 'O'==c || 'U'==c || 'a'==c || 'e'==c || 'i'==c || 'o'==c || 'u'==c)
+ {
+   printf("vowele");
+   }
+   else
+   {
+   printf("consonant");
+   }
+   }
