4f2a590a86438da0de178b094535e6accdcbbcdb
HEMALATHA <hemalathamuthukrishnan586@gmail.com>
2017-09-19 10:22:15 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+  int count,n;
+  scanf("%d",&n);
+ for(count=1; count<=n; count++) 
+  {
+   if(counter%2 == 0) 
+    {
+         printf("%d ",count); 
+    } 
+   } 
+}
