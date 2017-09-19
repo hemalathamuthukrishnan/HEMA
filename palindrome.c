dc4a1fb92578b7184b110c5634fb5c2481a1e885
HEMALATHA <hemalathamuthukrishnan586@gmail.com>
2017-09-19 09:55:11 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int n,rev=0,remainder,num;
+ scanf("%d", &n);
+ num = n;
+ while( n!=0 )
+    {
+        remainder = n%10;
+        rev = rev*10 + remainder;
+        n /= 10;
+    }
+if (originalInteger == reversedInteger)
+        printf("%d is a palindrome",num);
+    else
+        printf("%d is not a palindrome",num);
+}
